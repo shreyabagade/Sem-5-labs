e1ff173b4d38a0809cc5da505c87084f2092b848
Shreya Bagade <111302123+shreyabagade@users.noreply.github.com>
2023-10-30 06:26:46 +0530

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,11 @@
+import json
+import boto3
+
+
+def lambda_handler(event, context):
+    client_dynamo-boto3.resource('dynamodb') #same as coloumn names and keep all string
+    table-client_dynamo.Table('Student')
+    response-table.put_item(Item-event)
+    return response
+    
+   
